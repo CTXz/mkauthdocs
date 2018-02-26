CTXz/mkauthdocs

commit 8dd4e9e7b2eed01ad7b337c3fb5f28301d12eb15

Login: Display alert on failed login attempt
Signed-off-by: Patrick Pedersen <ctx.xda@gmail.com>

mkauthdocs/templates/login_template.php
@@ -1,5 +1,10 @@
 <?php
 session_start();
+
+if (isset($_POST['submit'])) {
+  print('<div class="alert alert-danger" role="alert"><center>The username or password is invalid</center></div>');
+}
+
 if ($_POST["uname"] == "{username}" && $_POST["pwd"] == "{password}") {
   $_SESSION['login'] = true;
   header("Location: ".$_GET["redirect"]);

setup.py
@@ -28,7 +28,7 @@ long_description = "Mkauthdocs is a tool specifically made to implement simple a
 
 setup(
     name='mkauthdocs',
-    version='0.1.3',
+    version='0.1.4',
     description='An authentication tool for mkdocs builds',
     long_description=long_description,
     url='https://github.com/ctxz/mkauthdocs',

commit 64d724ee84dc6fd1378516fad513f313f9ff8471

Login: Prevent 'headers already sent' error
Fixes error caused by 8dd4e9e7b2eed01ad7b337c3fb5f28301d12eb15 due to premature printing

Signed-off-by: Patrick Pedersen <ctx.xda@gmail.com>

mkauthdocs/templates/login_template.php
@@ -1,14 +1,15 @@
 <?php
 session_start();
 
-if (isset($_POST['submit'])) {
-  print('<div class="alert alert-danger" role="alert"><center>The username or password is invalid</center></div>');
-}
-
 if ($_POST["uname"] == "{username}" && $_POST["pwd"] == "{password}") {
   $_SESSION['login'] = true;
   header("Location: ".$_GET["redirect"]);
 }
+
+if (isset($_POST['submit'])) {
+  print('<div class="alert alert-danger" role="alert"><center>The username or password is invalid</center></div>');
+}
+
 ?>
 
 <!DOCTYPE html>

setup.py
@@ -28,7 +28,7 @@ long_description = "Mkauthdocs is a tool specifically made to implement simple a
 
 setup(
     name='mkauthdocs',
-    version='0.1.4',
+    version='0.1.5',
     description='An authentication tool for mkdocs builds',
     long_description=long_description,
     url='https://github.com/ctxz/mkauthdocs',