8dd4e9e7b2eed01ad7b337c3fb5f28301d12eb15
Patrick Pedersen <ctx.xda@gmail.com>
2018-02-26 16:05:05 +0100

    Login: Display alert on failed login attempt
    Signed-off-by: Patrick Pedersen <ctx.xda@gmail.com>

--- mkauthdocs/templates/login_template.php
+++ mkauthdocs/templates/login_template.php
@@ -1,5 +1,10 @@
 <?php
 session_start();
+
+if (isset($_POST['submit'])) {
+  print('<div class="alert alert-danger" role="alert"><center>The username or password is invalid</center></div>');
+}
+
 if ($_POST["uname"] == "{username}" && $_POST["pwd"] == "{password}") {
   $_SESSION['login'] = true;
   header("Location: ".$_GET["redirect"]);
