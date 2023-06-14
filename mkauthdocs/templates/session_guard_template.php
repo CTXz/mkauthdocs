a82dd18e2002abc9ee2bcf7677c7a9d603663bd7
Sander de Kroon <sanderdekroon@live.nl>
2023-06-14 12:08:23 +0200

    fix: accessing undefined session key and dirname

--- mkauthdocs/templates/session_guard_template.php
+++ mkauthdocs/templates/session_guard_template.php
@@ -1,13 +1,8 @@
 <?php
 	session_start();
-	if (!$_SESSION['login']) {
-		$dirname = $_SERVER['REQUEST_URI'];
-
-		if (substr($dirname, -1) != '/') {
-			$dirname=dirname($dirname).'/';
-		} else {
-			$dirname = preg_replace('~/+~', '/', $dirname);
-		}
+	if (! isset($_SESSION['login']) || ! $_SESSION['login']) {
+		$dirname = $_SERVER['REQUEST_URI'] ?? '';
+        $dirname = preg_replace('/index.php$/', '', $dirname);
 
 		header("Location: ".$dirname."{calibration}login.php?redirect={redirect}");
 	}
