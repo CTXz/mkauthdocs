b6f625a3b4a35a2c57ec844d1e71f2adfe30ec80
Sander de Kroon <sanderdekroon@live.nl>
2023-06-14 12:56:37 +0200

    fix: whitespace

--- mkauthdocs/templates/session_guard_template.php
+++ mkauthdocs/templates/session_guard_template.php
@@ -1,9 +1,9 @@
 <?php
-	session_start();
-	if (! isset($_SESSION['login']) || ! $_SESSION['login']) {
-		$dirname = $_SERVER['REQUEST_URI'] ?? '';
-        $dirname = preg_replace('/index.php$/', '', $dirname);
+  session_start();
+  if (! isset($_SESSION['login']) || ! $_SESSION['login']) {
+    $dirname = $_SERVER['REQUEST_URI'] ?? '';
+    $dirname = preg_replace('/index.php$/', '', $dirname);
 
-		header("Location: ".$dirname."{calibration}login.php?redirect={redirect}");
-	}
+    header("Location: ".$dirname."{calibration}login.php?redirect={redirect}");
+  }
 ?>
